f32bb070bf1b773bc0241c55a873616c60247172
BANG <BANG@LAPTOP-7MH8S9EP>
2020-09-19 14:00:56 +0700

    19/09/2020 update chorme

--- Check_Environment/Topic_01_Check_Environment.java
+++ Check_Environment/Topic_01_Check_Environment.java
@@ -1,6 +1,7 @@
 package Check_Environment;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Topic_01_Check_Environment {
@@ -9,7 +10,10 @@ static WebDriver driver;
 	public static void main (String[] args) {
 		System.setProperty("webdriver.gecko.driver",".\\lib\\Brower\\geckodriver.exe");
 		
+		System.setProperty("webdriver.chrome.driver",".\\lib\\Brower\\chromedriver.exe");
+		
 		driver = new FirefoxDriver();
+		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 	}
 }
